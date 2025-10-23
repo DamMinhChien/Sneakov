114c85bc5142bd774e3b8e0ad7397a43959a7cd9
Đàm Minh Chiến <damminhchien220204@gmail.com>
2025-10-23 10:43:38 +0700

    add feature Onboarding

---
+++ app/src/main/java/com/magento/sneakov/presentation/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.magento.sneakov.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// 🎨 LIGHT MODE COLORS
+val BluePrimary = Color(0xFF0D6EFD)     // Xanh chủ đạo
+val White = Color(0xFFFFFFFF)           // Trắng
+val BackgroundLight = Color(0xFFF7F7F9) // Nền sáng
+val TextDark = Color(0xFF2B2B2B)        // Chữ chính
+val TextGray = Color(0xFF707881)        // Chữ phụ
+val GrayMedium = Color(0xFF6A6A6A)      // Xám trung tính
+val AccentRed = Color(0xFFF87265)       // Màu nhấn / cảnh báo
+
+// 🌑 DARK MODE COLORS
+val BluePrimaryDark = Color(0xFF0A58CA)     // Xanh đậm hơn chút, nổi bật trên nền tối
+val BackgroundDark = Color(0xFF121212)      // Nền tối chính
+val SurfaceDark = Color(0xFF1E1E1E)         // Card, AppBar, BottomSheet
+val TextLight = Color(0xFFEAEAEA)           // Chữ sáng chính
+val TextLightGray = Color(0xFF9CA3AF)       // Chữ phụ
+val GrayDark = Color(0xFF444444)            // Viền hoặc phân cách
+val AccentRedDark = Color(0xFFF86D5A)       // Đỏ hơi ấm hơn để bớt chói
--- app/src/main/java/com/magento/sneakov/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/magento/sneakov/presentation/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
-package com.magento.sneakov.ui.theme
+package com.magento.sneakov.presentation.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -12,15 +11,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+
+    primary = BluePrimaryDark,
+    background = BackgroundDark,
+    surface = SurfaceDark,
+    onBackground = TextLight,
+    onSurface = TextLight,
+    error = AccentRedDark
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = BluePrimary,
+    background = BackgroundLight,
+    surface = White,
+    onBackground = TextDark,
+    onSurface = TextDark,
+    error = AccentRed,
+    secondary = GrayMedium
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
